93e6e9b1e495d8c1f118f3bf70aaa14950869cac
Deblina785 <60137113+Deblina785@users.noreply.github.com>
2021-03-11 11:53:14 +0530

    Create about_us.php

---
+++ about_us.php
@@ -0,0 +1,145 @@
+<?php
+
+require "includes/common.php";
+
+?>
+
+<!DOCTYPE html>
+
+
+
+<html>
+
+    <head>
+
+        <title>About Us | Time World</title>
+
+        <link rel="shortcut icon" href="img\srtcticon.png" type="image/png">
+
+        
+
+        <meta charset="UTF-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link href="css/bootstrap.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+
+        <link href="css/style.css" rel="stylesheet">
+
+        <!-- jQuery -->
+
+        <script src="js/jquery.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+
+        <script src="js/bootstrap.min.js"></script>
+
+    </head>
+
+    
+
+      
+
+<body>
+
+       <!-- Header -->
+
+    <?php include 'includes/header.php'; ?>
+
+    <br>
+
+        <div class="container">
+
+            <div class="col-sm-4">
+
+                <div class="panel panel-default">
+
+                   <div class="panel-body">
+
+                    <h3 style="color:orange" class="color">WHO WE ARE</h3>
+
+                    <img src="./img/about-img.jpg" alt="about image"><br><br>
+
+                    <p>Welcome to Time World!!! We're dedicated to giving you the very best of watches, with a focus on [three characteristics, ie: dependability, customer service and uniqueness.]Founded in 1988 by Ms.JENIFER HILL, Time World has come a long way from its beginning. When Ms.Jenifer first started out,her passion for  providing the best equipment to the public drove her to [action, ie: do intense research, quit her day job], and gave her the impetus to turn hard work and inspiration into to a booming online store. We now serve customers all over places and are thrilled to be a part of the quirky, eco-friendly, fair trade wing of the  fashion, stylish, watch industry.We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don't hesitate to contact us.<p>
+
+                </div>
+
+                </div>
+
+            </div>
+
+            
+
+            
+
+            <div class="col-sm-4">
+
+                <div class="panel panel-default">
+
+                   <div class="panel-body">
+
+                    <h3 style="color:orange" class="color">OUR HISTORY</h3><br><br>
+
+                    <h4 style="color:blue">1988&#45;</h4>
+
+                    <p> In 1988,Ms.Jenifer first started out,her passion for  providing the best equipment to the public drove her to [action, ie: do intense research, quit her day job], and gave her the impetus to turn hard work and inspiration into to a booming online store.Her idea was simple and as she was facinated to watches hence converted her passion into buisness.</p>
+
+                    <br>
+
+                    <h4 style="color:blue">2002&#45;</h4>
+
+                    <p>In 2002,the Time World came up with increasing varities meeting the demands of the people.With increasing varities of watches with varies price ranges and also to moving on to become one of the biggest and renowed luxirious online watch company,D-Store has went a long way, providing happiness to people through their watches.</p>
+
+                    <br>
+
+                    <h4 style="color:blue">2016&#45;</h4>
+
+                    <p>In 2016,Time World got prestigious position within the top luxirious and renowed for best quality watches with with range of variety products ranging within different price ranges and meeting the up to date technological advancement. </p>
+
+                    </div>
+
+                </div>
+
+                </div>
+
+            
+
+            
+
+            <div class="col-sm-4">
+
+                <div class="panel panel-default">
+
+                   <div class="panel-body">
+
+                    <h3 style="color:orange" class="color">OPPORTUNITIES</h3><br><br>
+
+                        <b>Available Roles</b>
+
+                            <ol>
+
+                                <li>Jr./Sr. Web Developer [Full Time Role + also available as a 6 Months Internships]</li>
+
+                                <li>Bussiness Apprentise [6 Months Internships]</li>
+
+                                <li>Manager at backend operations[Full Time Role + also available as a 6 Months Internships]</li>
+
+                            </ol>
+
+                        </div>
+
+                </div>
+
+            </div>
+
+            
+
+        </div>
+
+        <?php include 'includes/footer.php'; ?>
+
+    </body>
+
+</html>
